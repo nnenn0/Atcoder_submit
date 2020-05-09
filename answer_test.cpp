9df2854ec9fe02418329d0167d643fe3db52fdaf
nnenn0 <geek.branch.info@gmail.com>
2020-05-09 11:53:23 +0900

    init

---
+++ answer_test.cpp
@@ -0,0 +1,36 @@
+//#include <gtest/gtest.h>
+#include "answer.hpp"
+
+int main() {
+    answer(123);
+
+    answer("string");
+
+    answer(std::string("str"));
+
+    std::array<std::string, 3> arr_s{"first", "second", "third"};
+    answer(arr_s);
+
+    std::vector<int> vec{11, 13, 17, 19, 22};
+    answer(vec);
+
+    answer(std::vector<int>{1, 2, 3});
+
+    answer(std::vector<int>{1, 2, 3}, std::vector<int>{1, 2, 3});
+
+    std::vector<double> vec_d {1.5, 2.0, 4.5};
+    answer(vec_d);
+
+    answer(123, vec, vec_d, vec);
+
+    std::vector<std::vector<int>> table{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    answer(table);
+
+    std::array<std::array<std::string, 3>, 2> table_a{"str", "ing", "str"};
+    table_a.at(1) = {"test", "str", "ing"};
+    answer(table_a);
+
+    std::vector<std::vector<std::string>> vec_s;
+    vec_s.push_back(std::vector<std::string>{"str", "str", "str"});
+    answer(vec_s);
+}
